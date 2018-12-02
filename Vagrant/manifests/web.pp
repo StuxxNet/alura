45825dc8acf2d0c79ad4385855d2896ec41f04a4
Ramon Borges <F0FP559@dc.com.br>
2018-12-02 21:27:55 -0200

    Added unzip package to Puppet manifest

--- Vagrant/manifests/web.pp
+++ Vagrant/manifests/web.pp
@@ -2,7 +2,7 @@ exec { "apt-get update":
     command => "/usr/bin/apt-get update"
 }
 
-package { ["openjdk-7-jre", "tomcat7"]:
+package { ["openjdk-7-jre", "tomcat7", "unzip"]:
     ensure => installed,
     require => Exec["apt-get update"],
 }
