0ce200872abda30b528a578d2f8284ec051378cb
Ramon Borges <RamonBoorges@gmail.com>
2018-12-24 12:07:13 -0200

    Mudei o if pra dar dicas sobre o chute do usuário

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -8,9 +8,14 @@ chute_usuario = int(input("Digite o seu número: "))
 
 print("Você digitou: ", chute_usuario)
 
-if (numero_secreto == chute_usuario):
-    print("Você acertou o número!")
-else:
-    print("Você errou o número!")
+acertou = chute_usuario == numero_secreto
+maior   = chute_usuario > numero_secreto
 
+if (acertou):
+    print("Você acertou o número!")
+elif (maior):
+    print("Você chutou um número maior")
+else:
+    print("Você chutou um número menor")
+    
 print("Fim do jogo")
