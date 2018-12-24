e23e596c43973c2428064fba8ad22ad918f3f799
Ramon Borges <RamonBoorges@gmail.com>
2018-12-24 14:58:49 -0200

    Adicionei check do intervalo do número

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -7,13 +7,16 @@ total_tentativas = 3
 
 for rodada in range(1, total_tentativas + 1):
     print("Rodada {} de {}".format(rodada, total_tentativas))
-    chute_usuario = int(input("Digite o seu número: "))
+    chute_usuario = int(input("Digite um número entre 1 e 100: "))
+    if (chute_usuario < 1 or chute_usuario > 100):
+        print("O chute não deve ser menor do que 1 nem maior do que 100")
+        continue
     print("Você digitou: ", chute_usuario)
-
     acertou = chute_usuario == numero_secreto
     maior   = chute_usuario > numero_secreto
     if (acertou):
         print("Você acertou o número!")
+        break
     elif (maior):
         print("Você chutou um número maior")
     else:
