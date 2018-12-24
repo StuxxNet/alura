6ca8ed27196108a46e962b93eb1be5610aecac33
Ramon Borges <RamonBoorges@gmail.com>
2018-12-24 11:55:48 -0200

    Fim da aula 2

--- Python/jogos/adivinhacao.py
+++ Python/jogos/adivinhacao.py
@@ -1,1 +1,16 @@
-print("Hello world!")
+print("*********************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute_usuario = int(input("Digite o seu número: "))
+
+print("Você digitou: ", chute_usuario)
+
+if (numero_secreto == chute_usuario):
+    print("Você acertou o número!")
+else:
+    print("Você errou o número!")
+
+print("Fim do jogo")
