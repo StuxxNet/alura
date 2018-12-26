de4e10aa5049905468cde055a5b06a9a7bec9a26
Ramon Borges <ramonboorges@gmail.com>
2018-12-26 16:33:24 -0200

    Fim do curso 2

--- Python/jogos/forca.py
+++ Python/jogos/forca.py
@@ -1,11 +1,61 @@
+import random
+
 def imprime_comeco():
     print("*********************************")
     print("Bem vindo ao jogo da forca!")
     print("*********************************")
-    print("Fim do jogo")
+
+def ler_arquivo(caminho):
+    lista_palavras = []
+    with open(caminho) as arquivo:
+        for linha in arquivo:
+            lista_palavras.append(linha.strip().upper())
+    return lista_palavras
+
+def define_palavra(lista_palavras):
+    return lista_palavras[random.randrange(0,len(lista_palavras) - 1)]
+
+def retorna_chute():
+    return input("\nDigite uma letra: ").strip().upper()
+
+def preenche_resultado(palavra):
+    return ["_" for letra in palavra]
+
+def imprime_palavra(resultado):
+    for letra in resultado:
+        print(letra, sep=" ", end=" ")
+    print("\n")
+
+def resolve_jogo(palavra):
+    enforcou = False
+    acertou = False
+    erros = 0
+    resultado = preenche_resultado(palavra)
+    imprime_palavra(resultado)
+    while (not enforcou and not acertou):
+        chute = retorna_chute()
+        index = 0
+        if (chute in palavra):
+            for letra in palavra:
+                if (chute == letra):
+                    resultado[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("Você errou. Jogadas restantes:{}".format(6 - erros))
+        acertou = "_" not in resultado
+        enforcou = erros == 6
+        imprime_palavra(resultado)
+    if (acertou):
+        print("Você ganhou!")
+    if (enforcou):
+        print("Você perdeu!")
 
 def jogar():
-    print("Aqui começa o jogo")
+    imprime_comeco()
+    lista_palavras = ler_arquivo("c:/Users/f0fp559/Documents/alura/Python/jogos/palavras.txt")
+    palavra = define_palavra(lista_palavras)
+    resolve_jogo(palavra)
 
 if(__name__ == "__main__"):
     jogar()
--- Python/jogos/jogos.py
+++ Python/jogos/jogos.py
@@ -4,10 +4,12 @@ import forca
 print("*********************************")
 print("Escolha o seu jogo!")
 print("*********************************")
-print("1 - Adivinhação\n2 - Forca")
+print("1 - Adivinhação\n2 - Forca\n3 - Sair")
 jogo = int(input("Digite a opção: "))
 
 if (jogo == 1):
     adivinhacao.jogar()
 elif (jogo == 2):
     forca.jogar()
+else:
+    exit()
